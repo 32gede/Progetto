67eb6598eb70b32ad540b0e60cdeb4a84dcfd4cd
Jk-srt <alessandrosartori368@gmail.com>
2024-07-17 18:28:10 +0200

    Sistemazione grafica del codice (più bello da leggere)

--- app.py
+++ app.py
@@ -1,34 +1,34 @@
-from flask import Flask, make_response,render_template,request,redirect,session
-from flask import Flask, make_response,render_template,redirect,request
+from flask import Flask, make_response, render_template, request, redirect, session
 import requests
 from flask_session import Session
-import requests
 import sqlalchemy as sq
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import Column, String, Integer, select
-from sqlalchemy.ext.declarative import declarative_base
+
 Base = declarative_base()
+
 class User(Base):
-      __tablename__ = 'login'  # Specify the table name
-      id = Column(Integer, primary_key=True, nullable=False)
-      email = Column(String(255), nullable=False)
-      pssw = Column(String(255), nullable=False)
+    __tablename__ = 'login'  # Specify the table name
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String(255), nullable=False)
+    pssw = Column(String(255), nullable=False)
+
 def connect():
   engine = sq.create_engine('postgresql+psycopg2://Prova:1234@localhost/Prova')  
   session= sessionmaker(bind=engine)
   return session()
 
-app=Flask(__name__,template_folder='templates')
+app = Flask(__name__, template_folder='templates')
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
 @app.route('/')
 def index():
-  if session.get('id') :
-     print (session.get('id'))
-  else:
-    print('NESSUN cookie')
-  return render_template('index.html')
+    if session.get('id'):
+        print(session.get('id'))
+    else:
+        print('NESSUN cookie')
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -36,15 +36,15 @@ def login():
         username = request.form['email']
         password = request.form['password']
         print(username, password)
-        db_session =connect()
-        result=db_session .query(User).filter_by(email=username, pssw=password)
+        db_session = connect()
+        result = db_session.query(User).filter_by(email=username, pssw=password)
         if result.first():
-          user_id = result.first().id
-          session['id']=user_id
-          return redirect('/')
+            user_id = result.first().id
+            session['id'] = user_id
+            return redirect('/')
         else:
-          print('NO')
-          return render_template('login.html', error='Invalid username or password')
+            print('NO')
+            return render_template('login.html', error='Invalid username or password')
     else:
         return render_template('login.html')
 
@@ -58,32 +58,34 @@ def login():
 @app.route('/set_data',methods=['POST']) 
 def set_data(): 
     if request.method == 'POST':
-      user_id = request.json['id']
-      response=make_response('')
-      response.set_cookie('id', str(user_id))
-      print("prova")
-      return response
+        user_id = request.json['id']
+        response = make_response('')
+        response.set_cookie('id', str(user_id))
+        print("prova")
+        return response
     else:
-      return render_template('login.html')
+        return render_template('login.html')
 
-def getcookie(): 
-    id = request.cookies.get('id') 
-    return id 
-
+def getcookie():
+    id = request.cookies.get('id')
+    return id
 
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
-   if request.method == 'POST':
+    if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
         user = User(email=email, pssw=password)
         session = connect()
-        result=session.query(User).filter_by(email=email)
+        result = session.query(User).filter_by(email=email)
         if not result.first():
             session.add(user)
             session.commit()
             return redirect('/login')
         else:
             return render_template('registration.html', error='User already exists')
-   else:
-       return render_template('registration.html')
+    else:
+        return render_template('registration.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
