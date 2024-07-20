ce9813c4f310629cb0761ad78aa19a19881040a2
Jk-srt <alessandrosartori368@gmail.com>
2024-07-20 16:48:22 +0200

    Aggiunto Mapped e mapped_column, che è una funzione di sqlAlchemy 2.0 (più leggibile)

--- models.py
+++ models.py
@@ -3,19 +3,28 @@ from sqlalchemy.orm import relationship, declarative_base, Mapped, mapped_column
 
 Base = declarative_base()
 
-class User(Base):
+class User_buyers(Base):
     __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
     email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
+    role: Mapped[str] = mapped_column(String(255), nullable=False)
     # Aggiungi altri campi necessari
+
+class User_sellers(Base):
+    __tablename__ = 'user_sellers'
+
+    id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), primary_key=True)
+    # Aggiungi campi specifici per i venditori
+    seller_rating: Mapped[int] = mapped_column(Integer, nullable=False)
 
 class Product(Base):
     __tablename__ = 'products'
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(255), nullable=False)
-    description = Column(String(255), nullable=True)
-    price = Column(Integer, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    description: Mapped[str] = mapped_column(String(255), nullable=True)
+    price: Mapped[int] = mapped_column(Integer, nullable=False)
+    quantity: Mapped[int] = mapped_column(Integer, nullable=False)
     # Aggiungi altri campi necessari
