a67ba0c3ef1e46ef402e9f71c726544870f2ff0b
Federico Riato <126761136+32gede@users.noreply.github.com>
2024-08-12 18:30:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from models import User
 from flask_session import Session
@@ -5,41 +6,37 @@ from routes import main_routes
 from database import init_db, get_db_session
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
-# from flask_talisman import Talisman - Permette di configurare Content Security Policy (CSP) per proteggere
-#                                       l'applicazione da attacchi XSS (Cross-Site Scripting) e per configurare altre
-#                                       politiche di sicurezza (tipo https).
 
 app = Flask(__name__, template_folder='templates')
 app.jinja_env.autoescape = True
+
+# Configure the upload folder
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Ensure the uploads directory exists
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+# Configure session
 app.config['SESSION_TYPE'] = 'filesystem'
-# csrf = CSRFProtect(app)
 Session(app)
 
-'''
-# Configura CSP per permettere stili e script da specifiche fonti
-csp = {
-    'default-src': "'self'",
-    'script-src': ["'self'", 'https://cdn.jsdelivr.net'],
-    'style-src': ["'self'", 'https://cdn.jsdelivr.net'],
-    'font-src': ["'self'", 'https://cdn.jsdelivr.net'],
-}
-
-Talisman(app, content_security_policy=csp)  # Configura Talisman con la tua CSP personalizzata
-'''
-
+# Initialize database
 init_db()
 
+
+# Configure login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'main.login'
-
 
 @login_manager.user_loader
 def load_user(user_id):
     with get_db_session() as db_session:
         return db_session.query(User).get(int(user_id))
 
-
+# Register the main blueprint
 app.register_blueprint(main_routes)
 
 if __name__ == '__main__':
