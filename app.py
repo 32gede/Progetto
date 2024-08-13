781d02115fe0065bb3bf4a28ac19c62da10da1ae
Jk-srt <alessandrosartori368@gmail.com>
2024-08-13 15:58:10 +0200

    Cambiato query().get() con Session.get()

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ app.config['SESSION_TYPE'] = 'filesystem'
 
 # Abilita la protezione CSRF
 # Configura la chiave segreta
-# app.config['SECRET_KEY'] = 'your_secret_key_here'  # Cambia con una chiave segreta sicura e complessa
-# csrf = CSRFProtect(app)
+app.config['SECRET_KEY'] = 'your_secret_key_here'  # Cambia con una chiave segreta sicura e complessa
+csrf = CSRFProtect(app)
 Session(app)
 
 '''
@@ -52,7 +52,7 @@ login_manager.login_view = 'main.login'
 @login_manager.user_loader
 def load_user(user_id):
     with get_db_session() as db_session:
-        return db_session.query(User).get(int(user_id))
+        return db_session.get(User, int(user_id))  # Cambiato query().get() con Session.get()
 
 
 app.register_blueprint(main_routes)
